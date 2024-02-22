ac3ed1e269b6280c8addd04f589fa761432dfb0e
xernomm <rafaelrichie03@gmail.com>
2024-02-22 11:18:34 +0700

    navbar-fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,47 +7,75 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import '../style/navbar.css'; 
 import '../style/button.css'
 import logosvg from '../img/emobuddy logo.svg'
+import { Link } from 'react-scroll';
 
 export function CustomNavbar({ activePage }) {
   
   return (
     <>
-    <header className="fixed-top">
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container>
-        <Navbar.Brand href="#home"><span>
-          <img src={logosvg} alt="" className='nav-logo' />
-          </span></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-          <Nav.Link href="/" className={`nav-link ${activePage === "home" ? "active" : ""}`}>Home</Nav.Link>
-            <Nav.Link href='/link' className='nav-link'>About Us</Nav.Link>
-            <Nav.Link href='/link' className='nav-link'>Contact Us</Nav.Link>
-            <Nav.Link href='/link' className='nav-link'>Pricing</Nav.Link>
-            <Nav.Link href='/link' className='nav-link'>Why EmoBuddy?</Nav.Link>
+   <header className="fixed-top">
+      <Navbar expand="lg" className="bg-body-tertiary">
+        <Container>
+          <Navbar.Brand>
+            <img src={logosvg} alt="" className='nav-logo' />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Link
+                to="home"
+                smooth={true}
+                duration={500}
+                className={`nav-link ${activePage === "home" ? "active" : ""}`}
+              >
+                Home
+              </Link>
+              <Link
+                to="about"
+                smooth={true}
+                duration={500}
+                className={`nav-link ${activePage === "about" ? "active" : ""}`}
 
-            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item to="/">Action</NavDropdown.Item>
-              <NavDropdown.Item to="/">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown> */}
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Collapse className='justify-content-end signupnav'>
-          <div className="d-lg-flex">
-            <a type='button' href="/register" className="btn ">Sign Up</a>
-            <a type='button' href="/register" className="btn btn-ytp-primary ms-1">Login</a>
-          </div>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+              >
+                About Us
+              </Link>
+              <Link
+                to="contact"
+                smooth={true}
+                duration={500}
+                className={`nav-link ${activePage === "contact" ? "active" : ""}`}
+
+              >
+                Contact Us
+              </Link>
+              <Link
+                to="pricing"
+                smooth={true}
+                duration={500}
+                className={`nav-link ${activePage === "pricing" ? "active" : ""}`}
+
+              >
+                Pricing
+              </Link>
+              <Link
+                to="why-emobuddy"
+                smooth={true}
+                duration={500}
+                className={`nav-link ${activePage === "why-emobuddy" ? "active" : ""}`}
+
+              >
+                Why EmoBuddy?
+              </Link>
+            </Nav>
+          </Navbar.Collapse>
+          <Navbar.Collapse className='justify-content-end signupnav'>
+            <div className="d-lg-flex">
+              <a type='button' href="/register" className="btn ">Sign Up</a>
+              <a type='button' href="/register" className="btn btn-ytp-primary ms-1">Login</a>
+            </div>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </header>
     </>
   );
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { CustomNavbar } from "../components/Navbar"
 import '../style/base.css'
 import { LandingText } from "../components/LandingText"
@@ -12,36 +12,76 @@ import { AskUsForm } from "../components/AskUsForm"
 import { Footer } from "../components/Footer"
 
 export function Homepage(){
+    
+    const [activePage, setActivePage] = useState("home");
+    const sectionRefs = {
+        home: useRef(null),
+        about: useRef(null),
+        whyEmoBuddy: useRef(null),
+        pricing: useRef(null),
+        contact: useRef(null)
+    };
+
+    useEffect(() => {
+        const handleIntersections = (entries) => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    setActivePage(entry.target.id);
+                }
+            });
+        };
+
+        const observerOptions = {
+            root: null,
+            rootMargin: '0px',
+            threshold: 0.5 // Intersection ratio threshold for triggering callback
+        };
+
+        const observer = new IntersectionObserver(handleIntersections, observerOptions);
+
+        Object.values(sectionRefs).forEach(ref => {
+            if (ref.current) {
+                observer.observe(ref.current);
+            }
+        });
+
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
+
+
     return(
         <>
-        <CustomNavbar activePage={"home"}/>
+        <CustomNavbar activePage={activePage} />
         <div className="body-bg">
-            <Row>
+            <Row ref={sectionRefs.home} id="home">
                 <LandingText />
             </Row>
-            <Row className="newRow">
+            <Row ref={sectionRefs.about} id="about" className="newRow">
                 <Empowerment />
             </Row>
-            <Row >
+            <Row ref={sectionRefs.whyEmoBuddy} id="why-emobuddy">
                 <WhyEmoBuddy />
             </Row>
             <Row className="newRow mt-3">
                 <HowToSubscribe />
             </Row>
-            <Row className="newRow">
+            <Row ref={sectionRefs.pricing} id="pricing" className="newRow">
                 <SubscriptionPlans />
             </Row>
             <Row className="newRow">
                 <FrequentQuestions />
             </Row>
-            <Row className="mt-3">
+            <Row ref={sectionRefs.contact} id="contact" className="mt-3">
                 <AskUsForm />
             </Row>
             <Row className="newRow">
                 <Footer />
             </Row>
         </div>
-        </>
+    </>
         
     )
 }
